fc28e5d59ec9a301715bf0050155abe9cbecc0ae
Pawel Okrasa <pawelokrasa198@gmail.com>
2023-11-05 02:20:26 +0100

    working commenter api service

--- commenter-api/src/main.rs
+++ commenter-api/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use commenter_database::comments::Comment;
+use diesel::prelude::*;
+use rocket::{
+    response::status::NotFound,
+    serde::json::Json,
+};
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api", routes![get_comment]) // runs default on :8000
+}
+
+
+#[get("/comment/<id>")]
+fn get_comment(id: &str) -> Result<Json<commenter_database::comments::Comment>, NotFound<String>> {
+    // TODO: DB connection pool
+    commenter_database::schema::comments::dsl::comments
+        .find(id.to_owned())
+        .select(Comment::as_select())
+        .first(&mut commenter_database::establish_connection())
+        .map_err(|e| NotFound(e.to_string()))
+        .map(|comment| Json(comment))
 }
